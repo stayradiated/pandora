8eb3994ecd987027c704fa7c657ce5f2f71abf1c
George Czabania <george@czabania.com>
2014-08-20 07:36:07 +1200

    Use goroutines to make it way faster

--- pandora.go
+++ pandora.go
@@ -19,13 +19,76 @@ type songOutput struct {
 	Artist string `json:artist`
 }
 
-func (s *stationOutput) AddFeedback(feedback []responses.FeedbackResponse) {
-	for _, song := range feedback {
-		s.Songs = append(s.Songs, songOutput{
+func fetchStation(client *gopiano.Client, station responses.Station, result chan<- stationOutput) {
+	details, err := client.StationGetStation(station.StationToken, true)
+	if err != nil {
+		panic(err)
+	}
+
+	feedback := details.Result.Feedback.ThumbsUp
+
+	fmt.Println(station.StationName, len(feedback))
+
+	sOutput := stationOutput{
+		Name:  details.Result.StationName,
+		Songs: make([]songOutput, len(feedback)),
+	}
+
+	for i, song := range feedback {
+		sOutput.Songs[i] = songOutput{
 			Artist: song.ArtistName,
 			Name:   song.SongName,
-		})
+		}
 	}
+
+	result <- sOutput
+}
+
+func FetchStations(username, password string) ([]stationOutput, error) {
+	client, err := gopiano.NewClient(gopiano.AndroidClient)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = client.AuthPartnerLogin()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = client.AuthUserLogin(username, password)
+	if err != nil {
+		return nil, err
+	}
+
+	stationsList, err := client.UserGetStationList(false)
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]stationOutput, 0)
+	stations := stationsList.Result.Stations
+
+	result := make(chan stationOutput, 2)
+	expectedLength := len(stations)
+
+	for _, station := range stations {
+		if station.IsQuickMix || station.IsShared {
+			expectedLength -= 1
+			continue
+		}
+
+		go fetchStation(client, station, result)
+	}
+
+	for {
+		sOutput := <-result
+		output = append(output, sOutput)
+		if len(output) == expectedLength {
+			break
+		}
+	}
+
+	return output, nil
 }
 
 func main() {
@@ -38,48 +101,16 @@ func main() {
 	username := os.Args[1]
 	password := os.Args[2]
 
-	client, err := gopiano.NewClient(gopiano.AndroidClient)
+	output, err := FetchStations(username, password)
 	if err != nil {
 		panic(err)
-	}
-
-	_, _ = client.AuthPartnerLogin()
-	_, err = client.AuthUserLogin(username, password)
-	if err != nil {
-		panic(err)
-	}
-
-	stationsResult, err := client.UserGetStationList(false)
-	if err != nil {
-		panic(err)
-	}
-
-	output := make([]stationOutput, 0)
-	stations := stationsResult.Result.Stations
-
-	for i, station := range stations {
-		fmt.Println(i, "/", len(stations), station.StationName)
-
-		stationResult, err := client.StationGetStation(station.StationToken, true)
-		if err != nil {
-			panic(err)
-		}
-
-		station = stationResult.Result
-		sOutput := stationOutput{
-			Name:  station.StationName,
-			Songs: make([]songOutput, 0),
-		}
-
-		sOutput.AddFeedback(station.Feedback.ThumbsUp)
-
-		output = append(output, sOutput)
 	}
 
 	json, err := json.Marshal(output)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println(string(json))
 }
 
